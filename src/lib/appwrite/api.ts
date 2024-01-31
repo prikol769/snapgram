9f379d66a1e2f73ce2b2e9f0489e5ca4a3714402
prikol769 <prikol769@gmail.com>
2024-01-31 11:01:37 -0600

    added deletePost api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -314,3 +314,18 @@ export async function updatePost(post: IUpdatePost) {
     console.log(error);
   }
 }
+
+export async function deletePost(postId: string, imageId: string) {
+  if (!postId || !imageId) throw Error;
+
+  try {
+    await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      postId
+    );
+    return { status: "ok" };
+  } catch (error) {
+    console.log(error);
+  }
+}
